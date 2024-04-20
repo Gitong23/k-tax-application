e00321bbd074f40ea554af8824f12175c9e26869
Karn <gitong23@gmail.com>
2024-04-20 22:54:59 +0700

    extract function step cal tax

--- tax/handler.go
+++ tax/handler.go
@@ -23,31 +23,38 @@ func NewHandler() *Handler {
 	return &Handler{}
 }
 
-func calTax(netIncome float64, st []StepTax) float64 {
+func (s *StepTax) taxStep(amount float64) float64 {
+	if amount <= 0 {
+		return 0
+	}
+
+	if amount > (s.Max - s.Min) {
+		return (s.Max - s.Min) * s.Rate
+	}
+
+	return amount * s.Rate
+}
+
+func calTax(netIncome float64) float64 {
+
+	steps := []StepTax{
+		{0, 150000, 0},
+		{150000, 500000, 0.1},
+		{500000, 1000000, 0.15},
+		{1000000, 2000000, 0.20},
+		{2000000, math.MaxFloat64, 0.35},
+	}
+
 	result := 0.0
-	for _, s := range st {
-		if netIncome > (s.Max - s.Min) {
-			result += (s.Max - s.Min) * s.Rate
-			netIncome -= (s.Max - s.Min)
-			continue
-		}
-		result += netIncome * s.Rate
-		break
+	for _, s := range steps {
+		result += s.taxStep(netIncome)
+		netIncome -= s.Max - s.Min
 	}
 	return result
 }
 
 func newTax(netIncome float64, wht float64) *Tax {
-
-	return &Tax{
-		Tax: calTax(netIncome, []StepTax{
-			{0, 150000, 0},
-			{150000, 500000, 0.1},
-			{500000, 1000000, 0.15},
-			{1000000, 2000000, 0.20},
-			{2000000, math.MaxFloat64, 0.35},
-		}) - wht,
-	}
+	return &Tax{Tax: calTax(netIncome) - wht}
 }
 
 func (h *Handler) CalTax(c echo.Context) error {
