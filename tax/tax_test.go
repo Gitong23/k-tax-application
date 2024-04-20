fadf97d743056fa1a16fa6fc71762e7185a35e8f
Karn <gitong23@gmail.com>
2024-04-20 11:01:54 +0700

    implement test table

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -12,94 +12,72 @@ import (
 )
 
 func TestCalTax(t *testing.T) {
-	t.Run("Income 120k allowance 0 tax should be 0", func(t *testing.T) {
-		e := echo.New()
-		reqBody := TaxRequest{
-			TotalIncome: 120000.0,
-			WHT:         0.0,
-			Allowances: []Allowance{
-				{
-					AllowanceType: "donation",
-					Amount:        0.0,
-				},
-			}}
-		reqBodyStr, err := json.Marshal(reqBody)
-		if err != nil {
-			t.Errorf("error marshalling json: %v", err)
-		}
-		req := httptest.NewRequest(http.MethodPost, "/tax/calculation", strings.NewReader(string(reqBodyStr)))
 
-		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		rec := httptest.NewRecorder()
-		c := e.NewContext(req, rec)
-		c.SetPath("/tax/calculation")
+	//TODO: Implement to test table
+	tests := []struct {
+		name    string
+		reqBody TaxRequest
+		want    Tax
+	}{
+		{
+			name: "Income 120k allowance 0 tax should be 0",
+			reqBody: TaxRequest{
+				TotalIncome: 120000.0,
+				WHT:         0.0,
+				Allowances: []Allowance{
+					{
+						AllowanceType: "donation",
+						Amount:        0.0,
+					},
+				}},
+			want: Tax{Tax: 0},
+		},
+		{
+			name: "Income 500k allowance 0 tax should be 29000",
+			reqBody: TaxRequest{
+				TotalIncome: 500000.0,
+				WHT:         0.0,
+				Allowances: []Allowance{
+					{
+						AllowanceType: "donation",
+						Amount:        0.0,
+					},
+				}},
+			want: Tax{Tax: 29000},
+		},
+	}
 
-		handler := NewHandler()
-		handler.CalTax(c)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := echo.New()
 
-		want := Tax{Tax: 0}
-		gotJson := rec.Body.Bytes()
+			reqBodyStr, err := json.Marshal(tt.reqBody)
+			if err != nil {
+				t.Errorf("error marshalling json: %v", err)
+			}
 
-		var got Tax
+			req := httptest.NewRequest(http.MethodPost, "/tax/calculation", strings.NewReader(string(reqBodyStr)))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetPath("/tax/calculation")
 
-		if rec.Code != http.StatusOK {
-			t.Errorf("expected status code %d but got %d", http.StatusOK, rec.Code)
-		}
+			handler := NewHandler()
+			handler.CalTax(c)
 
-		err = json.Unmarshal(gotJson, &got)
-		if err != nil {
-			t.Errorf("error unmarshalling json: %v", err)
-		}
+			var got Tax
+			if rec.Code != http.StatusOK {
+				t.Errorf("expected status code %d but got %d", http.StatusOK, rec.Code)
+			}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("expected %v but got %v", want, got)
-		}
+			err = json.Unmarshal(rec.Body.Bytes(), &got)
+			if err != nil {
+				t.Errorf("error unmarshalling json: %v", err)
+			}
 
-	})
-
-	t.Run("Income 500k allowance 0 tax should be 29k", func(t *testing.T) {
-		e := echo.New()
-		reqBody := TaxRequest{
-			TotalIncome: 500000.0,
-			WHT:         0.0,
-			Allowances: []Allowance{
-				{
-					AllowanceType: "donation",
-					Amount:        0.0,
-				},
-			}}
-		reqBodyStr, err := json.Marshal(reqBody)
-		if err != nil {
-			t.Errorf("error marshalling json: %v", err)
-		}
-		req := httptest.NewRequest(http.MethodPost, "/tax/calculation", strings.NewReader(string(reqBodyStr)))
-
-		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		rec := httptest.NewRecorder()
-		c := e.NewContext(req, rec)
-		c.SetPath("/tax/calculation")
-
-		handler := NewHandler()
-		handler.CalTax(c)
-
-		want := Tax{Tax: 29000}
-		gotJson := rec.Body.Bytes()
-
-		var got Tax
-
-		if rec.Code != http.StatusOK {
-			t.Errorf("expected status code %d but got %d", http.StatusOK, rec.Code)
-		}
-
-		err = json.Unmarshal(gotJson, &got)
-		if err != nil {
-			t.Errorf("error unmarshalling json: %v", err)
-		}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("expected %v but got %v", want, got)
-		}
-
-	})
-
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("expected %v but got %v", tt.want, got)
+			}
+		})
+	}
 }
