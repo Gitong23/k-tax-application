bb9c11c9ecafa58bee4c04289ba379a40aa35052
Karn <gitong23@gmail.com>
2024-04-20 15:43:54 +0700

    extract function cal tax

--- tax/handler.go
+++ tax/handler.go
@@ -20,12 +20,29 @@ type StepTax struct {
 	Rate float64
 }
 
-var step = []StepTax{
-	{0, 150000, 0},
-	{150000, 500000, 0.1},
-	{500000, 1000000, 0.15},
-	{1000000, 2000000, 0.20},
-	{2000000, math.MaxFloat64, 0.35},
+func calTotalTax(netIncome float64) float64 {
+	step := []StepTax{
+		{0, 150000, 0},
+		{150000, 500000, 0.1},
+		{500000, 1000000, 0.15},
+		{1000000, 2000000, 0.20},
+		{2000000, math.MaxFloat64, 0.35},
+	}
+
+	sumTax := 0.0
+	for _, s := range step {
+
+		if netIncome > (s.Max - s.Min) {
+			sumTax += (s.Max - s.Min) * s.Rate
+			netIncome -= (s.Max - s.Min)
+			continue
+		}
+		sumTax += netIncome * s.Rate
+		fmt.Printf("sumTax: %v\n", sumTax)
+		break
+	}
+
+	return sumTax
 }
 
 func (h *Handler) CalTax(c echo.Context) error {
@@ -38,27 +55,5 @@ func (h *Handler) CalTax(c echo.Context) error {
 	//TODO:calculate income tax
 	incomeTax := taxRequest.TotalIncome - 60000
 
-	fmt.Printf("incomeTax: %.2f\n", incomeTax)
-	fmt.Printf("$$$$$$$$$$$$$$$$\n")
-	//TODO:calculate tax
-	sumTax := 0.0
-	for idx, s := range step {
-
-		if incomeTax > (s.Max - s.Min) {
-			sumTax += (s.Max - s.Min) * s.Rate
-			incomeTax -= (s.Max - s.Min)
-			fmt.Printf("range: %.2f - %.2f\n", s.Min, s.Max)
-			fmt.Printf("Amount Tax: %.2f\n", (s.Max-s.Min)*s.Rate)
-			fmt.Printf("sumTax: %.2f\n", sumTax)
-			fmt.Printf("incomeTax: %.2f\n", incomeTax)
-			fmt.Printf("---------%d-----------\n", idx+1)
-
-			continue
-		}
-		sumTax += incomeTax * s.Rate
-		fmt.Printf("sumTax: %v\n", sumTax)
-		break
-	}
-
-	return c.JSON(http.StatusOK, Tax{Tax: sumTax})
+	return c.JSON(http.StatusOK, Tax{Tax: calTotalTax(incomeTax)})
 }
