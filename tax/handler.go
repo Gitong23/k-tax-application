d586d22325697062c78f5d156ef25d9fd5e745cd
Karn  Kraiket <106549243+Gitong23@users.noreply.github.com>
2024-04-25 07:58:59 +0000

    serparate calculate tax level code module

--- tax/handler.go
+++ tax/handler.go
@@ -3,12 +3,10 @@ package tax
 import (
 	"encoding/csv"
 	"fmt"
-	"math"
 	"net/http"
 	"path/filepath"
 	"strconv"
 
-	"github.com/Gitong23/assessment-tax/helper"
 	"github.com/labstack/echo/v4"
 )
 
@@ -23,73 +21,12 @@ type Storer interface {
 	UpdateInitPersonalAllowance(amount float64) error
 }
 
-type StepTax struct {
-	Min  float64
-	Max  float64
-	Rate float64
-}
-
-var steps = []StepTax{
-	{0, 150000, 0},
-	{150000, 500000, 0.1},
-	{500000, 1000000, 0.15},
-	{1000000, 2000000, 0.20},
-	{2000000, math.MaxFloat64, 0.35},
-}
-
 type Err struct {
 	Message string `json:"message"`
 }
 
 func NewHandler(db Storer) *Handler {
 	return &Handler{store: db}
-}
-
-func (s *StepTax) taxStep(amount float64) float64 {
-	if amount <= 0 {
-		return 0
-	}
-
-	if amount > (s.Max - s.Min) {
-		return (s.Max - s.Min) * s.Rate
-	}
-
-	return amount * s.Rate
-}
-
-func calTax(netIncome float64) float64 {
-	result := 0.0
-	for _, s := range steps {
-		result += s.taxStep(netIncome)
-		netIncome -= s.Max - s.Min
-	}
-	return result
-}
-
-func taxLevel(netIncome float64) []TaxLevel {
-	var taxLevels []TaxLevel
-	for idx, s := range steps {
-
-		var level string
-		if idx == 0 {
-			level = fmt.Sprintf("0 - %s", helper.Comma(s.Max))
-		}
-
-		if idx == len(steps)-1 {
-			level = fmt.Sprintf("%s ขึ้นไป", helper.Comma(s.Min))
-		}
-
-		if idx > 0 && idx < len(steps)-1 {
-			level = fmt.Sprintf("%s - %s", helper.Comma(s.Min+1), helper.Comma(s.Max))
-		}
-
-		taxLevels = append(taxLevels, TaxLevel{
-			Level: level,
-			Tax:   s.taxStep(netIncome),
-		})
-		netIncome -= s.Max - s.Min
-	}
-	return taxLevels
 }
 
 func deductIncome(allReq []AllowanceReq, donation *Allowances, kreceipt *Allowances) float64 {
---
+++ tax/tax_level.go
@@ -0,0 +1,69 @@
+package tax
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/Gitong23/assessment-tax/helper"
+)
+
+type StepTax struct {
+	Min  float64
+	Max  float64
+	Rate float64
+}
+
+var steps = []StepTax{
+	{0, 150000, 0},
+	{150000, 500000, 0.1},
+	{500000, 1000000, 0.15},
+	{1000000, 2000000, 0.20},
+	{2000000, math.MaxFloat64, 0.35},
+}
+
+func taxLevel(netIncome float64) []TaxLevel {
+	var taxLevels []TaxLevel
+	for idx, s := range steps {
+
+		var level string
+		if idx == 0 {
+			level = fmt.Sprintf("0 - %s", helper.Comma(s.Max))
+		}
+
+		if idx == len(steps)-1 {
+			level = fmt.Sprintf("%s ขึ้นไป", helper.Comma(s.Min))
+		}
+
+		if idx > 0 && idx < len(steps)-1 {
+			level = fmt.Sprintf("%s - %s", helper.Comma(s.Min+1), helper.Comma(s.Max))
+		}
+
+		taxLevels = append(taxLevels, TaxLevel{
+			Level: level,
+			Tax:   s.taxStep(netIncome),
+		})
+		netIncome -= s.Max - s.Min
+	}
+	return taxLevels
+}
+
+func calTax(netIncome float64) float64 {
+	result := 0.0
+	for _, s := range steps {
+		result += s.taxStep(netIncome)
+		netIncome -= s.Max - s.Min
+	}
+	return result
+}
+
+func (s *StepTax) taxStep(amount float64) float64 {
+	if amount <= 0 {
+		return 0
+	}
+
+	if amount > (s.Max - s.Min) {
+		return (s.Max - s.Min) * s.Rate
+	}
+
+	return amount * s.Rate
+}
